31edeb5ec3dd220d178b729f928245ba534d05e5
harsh270511 <hm0093173@gmail.com>
2025-12-23 07:50:05 +0530

    added some files

---
+++ L33Tries/notes1.java
@@ -0,0 +1,54 @@
+//Insert & search in tries 
+package L33Tries;
+
+public class notes1 {
+  static class Node {
+    Node[] children = new Node[26];
+    boolean endOfWord = false;
+
+    Node() {
+      for (int i = 0; i < 26; i++) {
+        children[i] = null;
+      }
+    }
+
+  }
+
+  public static Node root = new Node();
+
+  // Insertation in trie & T.C = O(L) where L is the length of the largest word
+  public static void insert(String word) {
+    Node curr = root;
+    for (int level = 0; level < word.length(); level++) {
+      int idx = word.charAt(level) - 'a';
+      if (curr.children[idx] == null) {
+        curr.children[idx] = new Node();
+      }
+      curr = curr.children[idx];
+    }
+    curr.endOfWord = true;
+  }
+
+  // Search in trie & T.C=O(L) where L is the length of the largest word
+  public static boolean Search(String key) {
+    Node curr = root;
+    for (int level = 0; level < key.length(); level++) {
+      int idx = key.charAt(level) - 'a';
+      if (curr.children[idx] == null) {
+        return false;
+      }
+      curr = curr.children[idx];
+    }
+    return curr.endOfWord == true;
+  }
+
+  public static void main(String[] args) {
+    String[] word = { "the", "a", "there", "their", "any", "thee" };
+    // calling for inser
+    for (int i = 0; i < word.length; i++) {
+      insert(word[i]);
+    }
+    System.out.println(Search("their"));
+    System.out.println(Search("thee"));
+  }
+}
---
+++ L36Graph3/notes1.java
@@ -0,0 +1,78 @@
+//topological sort using bfs
+package L36Graph3;
+
+import java.util.*;
+
+public class notes1 {
+  static class Edge {
+    int src;
+    int dest;
+
+    public Edge(int s, int d) {
+      this.src = s;
+      this.dest = d;
+    }
+  }
+//this is the method to create a graph
+  public static void createGraph(ArrayList<Edge> graph[]) {
+    for (int i = 0; i < graph.length; i++) {
+      graph[i] = new ArrayList<>();
+    }
+    graph[2].add(new Edge(2, 3));
+
+    graph[3].add(new Edge(3, 1));
+
+    graph[4].add(new Edge(4, 0));
+    graph[4].add(new Edge(4, 1));
+
+    graph[5].add(new Edge(5, 0));
+    graph[5].add(new Edge(5, 2));
+  }
+
+  // helper fxn to calculate the indegree
+  public static void calInDegree(ArrayList<Edge> graph[], int[] idgre) {
+    for (int i = 0; i < graph.length; i++) {
+      int vertx = i;
+      for (int j = 0; j < graph[vertx].size(); j++) {
+        Edge e = graph[vertx].get(j);
+        idgre[e.dest]++;
+      }
+    }
+  }
+
+  //method for the actual kaam
+  public static void topoSort(ArrayList<Edge> graph[]) {
+    int[] idgre = new int[graph.length];
+    calInDegree(graph, idgre);
+    Queue<Integer> q = new LinkedList<>();
+
+    for (int i = 0; i < idgre.length; i++) {
+      if (idgre[i] == 0) {
+        q.add(i);
+      }
+    }
+    // bfs
+    while (!q.isEmpty()) {
+      int curr = q.remove();
+      System.out.print(curr + " ");
+
+      for (int i = 0; i < graph[curr].size(); i++) {
+        Edge e = graph[curr].get(i);
+        idgre[e.dest]--;
+        if (idgre[e.dest] == 0) {
+          q.add(e.dest);
+        }
+      }
+
+    }
+    System.out.println();
+  }
+
+  public static void main(String[] args) {
+    int V = 6;
+    ArrayList<Edge> graph[] = new ArrayList[V];
+    createGraph(graph);
+    topoSort(graph);
+
+  }
+}
